192b22472d5a71cabedbdaa760c36917cd12bc80
Oriana <rubyjpy@gmail.com>
2025-05-14 07:48:19 +0200

    Added some styling to the navbar and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-import './App.css'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-
-    </div>
-  )
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <nav className="bg-custom-blue p-8">
+          <ul className="flex justify-center space-x-6">
+            <li>
+              <Link
+                className="text-white hover:text-emerald-400 font-medium transition duration-300"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white hover:text-emerald-400 font-medium transition duration-300"
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-white hover:text-emerald-400 font-medium transition duration-300"
+                to="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="container mx-auto p-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
