04aef2fd3971b5ac337f0e05ffb11d5669209b80
Oriana <rubyjpy@gmail.com>
2025-05-14 07:18:26 +0200

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
 
     </div>
   )
---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact(){
+    return(
+        <div className="contact">
+            <h1>Contact</h1>
+            <p>If you have any questions, feel free to reach out!</p>
+        </div>
+    );
+}
+
+export default Contact;
